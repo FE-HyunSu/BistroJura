e109213f0f35e3bd0b44819f6af6db8b472d67e1
fe.hyunsu <fe.hyunsu@gmail.com>
2023-01-08 15:35:27 +0900

    feature : header, babelrc set.

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,9 +1,28 @@
-import { HeaderBox } from './style';
+import { HeaderBox } from "./style";
 
 const Header = () => {
   return (
     <>
-      <HeaderBox>header</HeaderBox>
+      <HeaderBox>
+        <div className="inner">
+          <h1>
+            <a href="/">BISTROJURA</a>
+          </h1>
+          <nav>
+            <ul>
+              <li>
+                <a href="">menu1</a>
+              </li>
+              <li>
+                <a href="">menu2</a>
+              </li>
+              <li>
+                <a href="">menu3</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </HeaderBox>
     </>
   );
 };
--- components/layout/header/style.tsx
+++ components/layout/header/style.tsx
@@ -1,7 +1,45 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HeaderBox = styled.header`
   display: block;
-  height: 5rem;
+  position: relative;
+  width: 100%;
+  height: 8rem;
   border: 0.1rem solid #eee;
+  .inner {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    max-width: 120rem;
+    margin: auto;
+    padding: 0 2rem;
+    box-sizing: border-box;
+    h1 {
+      flex: 1 auto;
+      a {
+        display: block;
+        height: 100%;
+        text-decoration: none;
+        font-size: 1.4rem;
+        color: #1d1d1d;
+      }
+    }
+    nav {
+      ul {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        li {
+          a {
+            padding: 0 1rem;
+            text-decoration: none;
+            font-size: 1.3rem;
+            color: #1d1d1d;
+          }
+        }
+      }
+    }
+  }
 `;
